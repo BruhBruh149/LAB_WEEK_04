a436efd7c0ba7f910554ee201d8b22db003b82da
Haikal Ibrahim Fathurrahman <90713429+BruhBruh149@users.noreply.github.com>
2025-09-16 13:46:17 +0700

    Commit No. 4 –Assignment

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -8,16 +8,24 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 val TABS_FIXED = listOf(
     R.string.starbucks_title,
     R.string.janjijiwa_title,
-    R.string.kopikenangan_title,
+    R.string.kopikenangan_title
 )
 
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
-    FragmentStateAdapter(fragmentManager, lifecycle) {
+val TABS_CONTENT = listOf(
+    R.string.starbucks_desc,
+    R.string.janjijiwa_desc,
+    R.string.kopikenangan_desc
+)
+
+class CafeAdapter(
+    fragment: Fragment
+) : FragmentStateAdapter(fragment) {
 
     override fun getItemCount(): Int = TABS_FIXED.size
 
     override fun createFragment(position: Int): Fragment {
-        // Semua tab akan memanggil fragment default (Hello blank fragment)
-        return CafeDetailFragment()
+        return CafeDetailFragment.newInstance(TABS_CONTENT[position])
     }
 }
+
+
