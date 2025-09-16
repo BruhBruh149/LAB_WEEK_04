4af03352f2db63af9acdbaddc91506f0f2d93c6c
Haikal Ibrahim Fathurrahman <90713429+BruhBruh149@users.noreply.github.com>
2025-09-16 13:46:17 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -1,18 +1,22 @@
 package com.example.lab_week_04
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 
 private const val TAB_CONTENT = "TAB_CONTENT"
 
 class CafeDetailFragment : Fragment() {
+    private var contentResId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            contentResId = it.getInt(TAB_CONTENT)
+        }
     }
 
     override fun onCreateView(
@@ -20,5 +24,20 @@ class CafeDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_cafe_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.content_description)?.text =
+            getString(contentResId)
+    }
+
+    companion object {
+        fun newInstance(@StringRes contentResId: Int) =
+            CafeDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(TAB_CONTENT, contentResId)
+                }
+            }
     }
 }
--- app/src/main/java/com/example/lab_week_04/CafeFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeFragment.kt
@@ -11,10 +11,6 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class CafeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,7 +23,8 @@ class CafeFragment : Fragment() {
 
         val viewPager = view.findViewById<ViewPager2>(R.id.view_pager)
         val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
-        val adapter = CafeAdapter(childFragmentManager, lifecycle)
+
+        val adapter = CafeAdapter(this) // ✅ cukup kirim "this"
 
         viewPager.adapter = adapter
 
@@ -36,3 +33,4 @@ class CafeFragment : Fragment() {
         }.attach()
     }
 }
+
